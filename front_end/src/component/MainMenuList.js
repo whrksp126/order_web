53f1c60a7a0b56eed6297cdcbeae1fd8104e569f
whrksp126 <whrksp126@naver.com>
2022-08-17 23:58:14 +0900

    menuitems

--- front_end/src/component/MainMenuList.js
+++ front_end/src/component/MainMenuList.js
@@ -1,60 +1,52 @@
 import React from 'react'
+import MenuItem from './MenuItem'
+
+let foodApi = 
+[
+  {
+    'foodName' : '짬짜면',
+    'foodPrice' : '9500',
+    'foodUrl' : 'dubblefood.png',
+  }, 
+  {
+    'foodName' : '짜장면',
+    'foodPrice' : '8500',
+    'foodUrl' : 'blacknoodle.png',
+  }, 
+  {
+    'foodName' : '짬뽕',
+    'foodPrice' : '9000',
+    'foodUrl' : 'rednoddle.png',
+  },
+  {
+    'foodName' : '미니탕수육',
+    'foodPrice' : '12000',
+    'foodUrl' : 'miniforkfry.png',
+  }, 
+  {
+    'foodName' : '군만두',
+    'foodPrice' : '7500',
+    'foodUrl' : 'frymandu.png',
+  },
+  {
+    'foodName' : '칸쇼새우',
+    'foodPrice' : '18000',
+    'foodUrl' : 'chillshrip.png',
+  }, 
+  {
+    'foodName' : '새우볶음밥',
+    'foodPrice' : '8500',
+    'foodUrl' : 'shrimprice.png',
+  }
+]
 
 const MainMenuList = () => {
-
-  const img_Url = "images/dubblefood.png"
-  let foodApi = 
-  [
-    {
-      'foodName' : '짬짜면',
-      'foodPrice' : '9500',
-      'foodUrl' : 'dubblefood.png',
-    }, 
-    {
-      'foodName' : '짜장면',
-      'foodPrice' : '8500',
-      'foodUrl' : 'blacknoodle.png',
-    }, 
-    {
-      'foodName' : '짬뽕',
-      'foodPrice' : '9000',
-      'foodUrl' : 'rednoddle.png',
-    },
-    {
-      'foodName' : '미니탕수육',
-      'foodPrice' : '12000',
-      'foodUrl' : 'miniforkfry.png',
-    }, 
-    {
-      'foodName' : '군만두',
-      'foodPrice' : '7500',
-      'foodUrl' : 'frymandu.png',
-    },
-    {
-      'foodName' : '칸쇼새우',
-      'foodPrice' : '18000',
-      'foodUrl' : 'chillshrip.png',
-    }, 
-    {
-      'foodName' : '새우볶음밥',
-      'foodPrice' : '8500',
-      'foodUrl' : 'shrimprice.png',
-    }
-  ]
 
   return (
     <div style={{height:"78vh", padding: "2rem", padding: "1em", justifyContent: "start", alignItems: "self-start", display: "flex", flexFlow: "wrap"}}>
       {
-        foodApi.map(food=> (
-          <div style={{ display:"inline-block", borderRadius:"2px" }}>
-            <div>
-              <img src={'images/' + food.foodUrl} style={{width:"200px", height:"180px"}} alt={"foodeImage"} />
-            </div>
-            <div style={{ textAlign:"center"}}>
-              <div>{food.foodName}</div>
-              <div>{food.foodPrice}원</div>
-            </div>
-          </div>    
+        foodApi.map((food, index)=> (
+          <MenuItem key={index} name={food.foodName} price={food.foodPrice} url={food.foodUrl} /> 
         ))
       }
     </div>
--- front_end/src/component/MenuItem.js
+++ front_end/src/component/MenuItem.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import './MenuItem.css'
 
-const MenuItem = () => {
+const MenuItem = ({name, price, url}) => {
   return (
-    <>
-    <h1>음식 아이템 각각</h1>
-    </>
+    <div className="menuItem">
+      <div>
+        <img src={'images/' + url}alt={"foodeImage"} />
+      </div>
+      <div className="menuInfo">
+        <div>{name}</div>
+        <div>{price}원</div>
+      </div>
+    </div>   
   )
 }
 
-export default MenuItem
+export default MenuItem;
