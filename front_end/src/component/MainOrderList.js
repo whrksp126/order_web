7d3a9744d12034605216022412fb65c9654f6251
whrksp126 <whrksp126@naver.com>
2022-08-21 03:02:18 +0900

    is_touch_device function 생성

--- front_end/src/component/MainOrderList.js
+++ front_end/src/component/MainOrderList.js
@@ -1,14 +1,16 @@
 import React, { useEffect, useState } from 'react'
 import './MainOrderList.css'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCartPlus } from "@fortawesome/free-solid-svg-icons";
+import { faCartPlus, faPlus, faMinus, faTrash } from "@fortawesome/free-solid-svg-icons";
 import { useDrop } from 'react-dnd';
 
 const MainOrderList = () => {
   const [selectedItem, setSelectedItem] = useState([]);
   const [newItem, setNewItem] = useState();
   const [{ canDrop, isOver }, drop] = useDrop(() => ({
-    //수락할 유형 - 문자열 또는 기호
+    // isOver 을 이용하여 선택 아이템을 드래그 하여 장바구니에 올라갔을 때를 설정할 수 있다.
+    // style={{ backgroundColor: isOver ? 'red' : 'white' }}
+    // 수락할 유형 - 문자열 또는 기호
     accept: 'BOX',
     // 수집할 소품
     collect: (monitor) => ({
@@ -32,22 +34,47 @@ const MainOrderList = () => {
     }else if(selectedItem.length !== 0){
       // 장바구니에 하나라도 있을 때
       for(let i=0;i<selectedItem.length;i++){
-        console.log('i,',i)
         if(selectedItem[i].name === newItem.name){
-          console.log('1번 실행됨')
-          selectedItem[i].count = selectedItem[i].count+1;
+          selectedItem[i].count = selectedItem[i].count + 1;
           return setSelectedItem([...selectedItem])
         } 
       }
+      // 선택 item이 장바구니에 없을 때(for문을 다 돌아도 같은게 나오지 않은 경우)
       return setSelectedItem([...selectedItem, newItem]);
     }
   },[newItem])
 
+  const plusItem = (item) => {
+    for(let i=0;i<selectedItem.length;i++){
+      if(selectedItem[i].name === item.name){
+        selectedItem[i].count = selectedItem[i].count + 1;
+        return setSelectedItem([...selectedItem])
+      } 
+    }
+  }
+  
+  const minusItem = (item) => {
+    for(let i=0;i<selectedItem.length;i++){
+      if(selectedItem[i].name === item.name){
+        if(selectedItem[i].count !== 1){
+          selectedItem[i].count = selectedItem[i].count - 1;
+          return setSelectedItem([...selectedItem]);
+        }else{
+          return setSelectedItem(selectedItem.filter(i => i.name !== item.name));
+        }
+      } 
+    }
+  }
+
+  const deleteItem = (item) => {
+    return setSelectedItem(selectedItem.filter(i => i.name !== item.name));
+  }
+
   return (
     <div className="MainOrderList">
 
-      <div style={{height: '20%', display:'flex', justifyContent: 'center', fontSize: '70px', color: 'white', textAlign: 'center', alignItems: 'center', border: '3px solid white', borderRadius: '20px', margin: '20px'}}>
-        <div ref={drop} style={{ backgroundColor: isOver ? 'red' : 'white' }}>
+      <div ref={drop} style={{height: '20%', display:'flex', justifyContent: 'center', fontSize: '70px', color: 'white', textAlign: 'center', alignItems: 'center', border: '3px solid white', borderRadius: '20px', margin: '20px'}}>
+        <div>
           <FontAwesomeIcon icon={faCartPlus} />
           <span style={{ display: 'block', fontWeight: '700', fontSize:'18px'}}>{canDrop ? '담 기' : '장바구니'}</span>
         </div>
@@ -60,7 +87,9 @@ const MainOrderList = () => {
               <th>이미지</th>
               <th>상품명</th>
               <th>가격</th>
+              <th></th>
               <th>수량</th>
+              <th></th>
               <th>버튼</th>
             </tr>
           </thead>
@@ -68,12 +97,26 @@ const MainOrderList = () => {
             {selectedItem.map((item, index) => (
             <tr key={index}>
               <td>{index+1}</td>
-              <td>{item.url}</td>
+              <td>
+                <img src={'images/'+ item.url} alt={"foodIcon"} style={{height:'5vh'}} />
+              </td>
               <td>{item.name}</td>
               <td>{item.price}</td>
+              <td>
+                <button onClick={()=> plusItem(item)}>
+                  <FontAwesomeIcon icon={faPlus} />
+                </button>
+              </td>
               <td>{item.count}</td>
               <td>
-                + - 삭제
+                <button onClick={()=> minusItem(item)}>
+                  <FontAwesomeIcon icon={faMinus} />
+                </button>
+              </td>
+              <td>
+                <button onClick={()=> deleteItem(item)}>
+                  <FontAwesomeIcon icon={faTrash} />
+                </button>
               </td>
             </tr>)
             )}
--- front_end/src/index.js
+++ front_end/src/index.js
@@ -5,12 +5,25 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Master from './Master';
 
 import { DndProvider } from 'react-dnd'
+import { TouchBackend } from 'react-dnd-touch-backend'
 import { HTML5Backend } from 'react-dnd-html5-backend'
+
+// 사용중인 디바이스 터치 사용여부 확인
+function is_touch_device() {
+  try {
+    document.createEvent("TouchEvent");
+    console.log('touch')
+    return true;
+  } catch (e) {
+    console.log('none touch')
+    return false;
+  }
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <DndProvider backend={HTML5Backend}>
+    <DndProvider  backend={is_touch_device() ? TouchBackend : HTML5Backend}>
       <Master />
     </DndProvider>
   </React.StrictMode>
